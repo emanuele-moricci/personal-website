a4495f7df472c7a62b612202ee2ae3fc2793e528
Emanuele Moricci <emanuele.moricci@gmail.com>
2025-04-23 20:04:27 +0200

    logo + marquee

---
+++ src/lib/components/layout/marquee/marquee.svelte
@@ -0,0 +1,18 @@
+<script lang="ts" module>
+export type MarqueeProps = { title: string };
+</script>
+
+<script lang="ts">
+    let { title }: MarqueeProps = $props();
+
+$effect(() => {
+	let charPos = 0;
+
+	const interval = setInterval(() => {
+		document.title = title.slice(charPos) + title.slice(0, charPos);
+		charPos = (charPos + 1) % title.length;
+	}, 250);
+
+	return () => clearInterval(interval);
+});
+</script>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,18 @@
 import "../app.css";
 
 import LocaleSwitcher from "@/components/layout/locale-switcher/locale-switcher.svelte";
+import Marquee from "@/components/layout/marquee/marquee.svelte";
+
+const title = " Hello, world! ";
 
 let { children } = $props();
 </script>
 
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+
 <LocaleSwitcher />
+<Marquee {title} />
 
 {@render children()}

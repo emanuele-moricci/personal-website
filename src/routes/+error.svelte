61431c3aabcc6acff2c2b34d7442a976c926c237
Emanuele Moricci <emanuele.moricci@gmail.com>
2025-04-29 23:33:35 +0200

    Better error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,7 +5,6 @@ import ContainerBox from "@/components/layout/container-box/container-box.svelte
 console.log(page);
 </script>
 
-<ContainerBox class="w-3/4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 py-3 px-4 pb-20 flex flex-col gap-4" title={`${page.status}`}>
-    <h1>{page.error?.message}</h1>
-    <span class="text-muted-foreground">ヽ(。_°)ノ</span>
+<ContainerBox class="w-3/4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 py-3 px-4 pb-20 flex flex-col gap-4" title={(page.status >= 300 && page.status < 400) ? '(ಠ_ಠ)' : (page.status >= 400 && page.status < 500) ? 'ヽ(。_°)ノ' : page.status >= 500 ? '(╯°□°)╯︵ ┻━┻' : ''}>
+    <h1><b class="text-muted-foreground">{page.status}</b> - {page.error?.message}</h1>
 </ContainerBox>

ae646d0ae323fe08ac4beab3006b7338324562b4
Emanuele Moricci <emanuele.moricci@gmail.com>
2025-05-01 15:33:48 +0200

    Added Healthcheck endpoint

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,20 @@
-import type { Handle } from '@sveltejs/kit';
-import { paraglideMiddleware } from '$lib/paraglide/server';
+import { paraglideMiddleware } from "$lib/paraglide/server";
+import type { Handle } from "@sveltejs/kit";
 
-const handleParaglide: Handle = ({ event, resolve }) => paraglideMiddleware(event.request, ({ request, locale }) => {
-	event.request = request;
+const handleParaglide: Handle = ({ event, resolve }) =>
+	paraglideMiddleware(event.request, ({ request, locale }) => {
+		event.request = request;
 
-	return resolve(event, {
-		transformPageChunk: ({ html }) => html.replace('%paraglide.lang%', locale)
+		return resolve(event, {
+			transformPageChunk: ({ html }) =>
+				html.replace("%paraglide.lang%", locale),
+		});
 	});
-});
 
-export const handle: Handle = handleParaglide;
+export const handle: Handle = ({ event, resolve }) => {
+	if (event.url.pathname === "/healthcheck") {
+		return new Response("ok");
+	}
+
+	return handleParaglide({ event, resolve });
+};

97f5823ca484a4316c1fe6e0c5d364a38dc30738
Emanuele Moricci <emanuele.moricci@gmail.com>
2025-05-01 12:58:03 +0200

    📱 Responsivity fixes - Pt. 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@ import ContainerBox from "@/components/layout/container-box/container-box.svelte
 import { GitBranch, Github, Lightbulb, Linkedin } from "@lucide/svelte";
 </script>
 
-<ContainerBox class="w-3/4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 py-3 px-4 pb-20" title={m.home_welcome()}>
+<ContainerBox class="w-4/5 md:w-3/4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 py-3 px-4 pb-20" title={m.home_welcome()}>
     <p class="text-muted-foreground mb-3">## {m.home_myNameIs()}</p>
     <pre class="ascii-art mb-5">
 $$$$$$$$\                                                       $$\
@@ -19,7 +19,7 @@ $$$$$$$$\ $$ | $$ | $$ |\$$$$$$$ |$$ |  $$ |\$$$$$$  |\$$$$$$$\ $$ |\$$$$$$$\
 \________|\__| \__| \__| \_______|\__|  \__| \______/  \_______|\__| \_______|
     </pre>
     <div class="horizontal-row">
-        <p class="text-justify w-0 !delay-[0.25s] animate-typing-slow text-sm">
+        <p class="text-justify lg:w-0 lg:!delay-[0.25s] animate-typing-slow text-sm !whitespace-break-spaces lg:!whitespace-nowrap">
             {m.home_hoAmI()}
             <span class="border-l border-4 ml-0.5 animate-caret-blink"></span>
         </p>
@@ -29,9 +29,9 @@ $$$$$$$$\ $$ | $$ | $$ |\$$$$$$$ |$$ |  $$ |\$$$$$$  |\$$$$$$$\ $$ |\$$$$$$$\
 
     <div class="mt-6 border px-3 py-2 horizontal-row">
         <h5>### <span class="bg-secondary p-1">{m.home_aboutMe()}</span></h5>
-        <div class="ml-auto flex justify-end items-center gap-4">
+        <div class="sm:ml-auto flex flex-col sm:flex-row sm:justify-end items-start sm:items-center sm:gap-4">
             <Button data-sveltekit-preload-data="hover" href="/experience" variant="ghost" class="p-1"><Lightbulb />{m.home_whatIKnow()}</Button>
-            <span class="height-full border"></span>
+            <span class="height-full border hidden sm:block"></span>
             <Button href="https://github.com/emanuele-moricci" target="_blank" variant="ghost" class="p-1"><Github />Github</Button>
             <Button href="https://www.linkedin.com/in/emanuele-moricci-777b44173/" target="_blank" variant="ghost" class="p-1"><Linkedin />Linkedin</Button>
         </div>
